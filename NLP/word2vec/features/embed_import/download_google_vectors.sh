8aeb4770947b92fa5565dfe33e3b9d3f05e8b8b8
Vlad K <nappa300@gmail.com>
2017-04-08 20:26:16 -0400

    #22 upgraded convert.py for new gensim version

--- NLP/word2vec/features/embed_import/download_google_vectors.sh
+++ NLP/word2vec/features/embed_import/download_google_vectors.sh
@@ -4,5 +4,6 @@ cd /var/www/git/Plants/NLP/word2vec/features/embed_import/;
 wget https://doc-0g-8s-docs.googleusercontent.com/docs/securesc/ha0ro937gcuc7l7deffksulhg5h7mbp1/o2tin09msf2l8dqq8qqfl8cuth1r0vun/1491688800000/06848720943842814915/*/0B7XkCwpI5KDYNlNUTTlSS21pQmM;
 gunzip -k 0B7XkCwpI5KDYNlNUTTlSS21pQmM;
 mv 0B7XkCwpI5KDYNlNUTTlSS21pQmM GoogleNews-vectors-negative300.bin;
-python convert.py;
+sudo pip3 install gensim; # installs it incase it is not there, incease its already there nothing happens.
+python3 convert_v2.py;
 cp GoogleNews-vectors-negative300.csv /var/www/git/Plants/NLP/word2vec/classify/0_data_source/GoogleNews-vectors-negative300.csv;
